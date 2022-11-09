7ad1d4fd2303d725b0d1c877ba0d7cab30eba260
qiaojunch <qiaojunch@gmail.com>
2022-11-08 21:08:51 -0800

    a8: add tuit controller file

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import HelloController from './controllers/hello-controller.js'
+import UserController from './controllers/users/users-controller.js'
+import TuitsController from './controllers/tuits/tuits-controller.js'
+
+const app = express()
+app.use(express.json())  // parse JSON from http request body
+
+TuitsController(app)
+HelloController(app)
+UserController(app)
+
+app.listen(4000)
---
+++ controllers/users/users-controller.js
@@ -0,0 +1,69 @@
+import people from './users.js';
+
+let users = people
+
+const UserController = (app) => {
+    app.get('/api/users', findUsers)
+    app.get('/api/users/:uid', findUsersById)      // Retrieve data from server
+    app.post('/api/users', createUser)             // Send data to server
+    app.delete('/api/users/:uid', deleteUser)     // Delete data from server
+    app.put('/api/users/:uid', updateUser)        // Update user info to server
+}
+
+// Send query parameters to server
+// ex: http://localhost:4000/api/users?type=STUDENT
+const findUsers = (req, res) => {
+    // retrieve type parameter from query
+    const type = req.query.type
+    // // if type param in query
+    if (type) {
+        const usersOfType = users.filter( u => u.type === type )
+        res.json(usersOfType)
+        return
+    }
+    // otherwise response with all users
+    res.json(users)
+}
+
+// Send path parameters to server
+// ex:  http://localhost:4000/api/users/123 
+const findUsersById = (req, res) => {
+    // get uid from request parameter map
+    const uid = req.params.uid;
+    // find user that matches the uid
+    const user = users.find(u => u._id === uid)
+
+    res.json(user)
+}
+
+// Map URL pattern to handler function
+const createUser = (req, res) => {
+    const newUser = req.body
+    console.log(newUser)
+    newUser._id = (new Date()).getTime() + ''
+    users.push(newUser)
+    res.json(newUser)
+}
+
+// delete data from server 
+const deleteUser = (req, res) => {
+    // get user id from path parameter
+    const uid = req.params['uid'] 
+    users = users.filter(u => u._id !== uid)
+    res.sendStatus(200)
+}
+
+// update data to server
+const updateUser = (req, res) => {
+    // get user id from path parameter
+    const uid = req.params.uid
+    // BODY includes the updated fields
+    const updates = req.body
+    // create new array for updated user
+    users = users.map( usr => 
+        usr._id === uid ? {...usr, ...updates} : usr)
+    res.sendStatus(200)
+}
+
+
+export default UserController;
