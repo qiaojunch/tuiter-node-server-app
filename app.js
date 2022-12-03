c40b656973a64da014a15d07029f5e481dd728a6
qiaojunch <qiaojunch@gmail.com>
2022-12-02 16:30:02 -0800

    a9: update api

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
 import express from 'express'
 import HelloController from './controllers/hello-controller.js'
 import UserController from './controllers/users/users-controller.js'
@@ -7,8 +9,8 @@ import mongoose from 'mongoose'
 
 //'mongodb+srv://cs5610:cs5610@cluster0.4l3ujxx.mongodb.net/?retryWrites=true&w=majority'
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-|| 'mongodb://localhost:27017/tuiter';
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+// || 'mongodb://localhost:27017/tuiter';
 mongoose.connect(CONNECTION_STRING);
 
 const app = express()
